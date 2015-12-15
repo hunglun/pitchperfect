a93ddc7f851552d74d95667f57762f6bcb399810
hunglun <hunglun.o@gmail.com>
2015-12-16 01:28:10 +0800

    Lesson 2 and 3: Add stop button, record button, recording label, navigation controller Action : disable/enable record button, hide/show stop button, show/hide recording label

--- pitchperfect/ViewController.swift
+++ pitchperfect/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+   
+    @IBOutlet weak var recordingInProgressLabel: UILabel!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    @IBOutlet weak var recordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // hide stop button
+        stopRecordingButton.hidden = true
+        recordButton.enabled = true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+  
+    @IBAction func recordAudio(sender: AnyObject) {
+        
+        stopRecordingButton.hidden = false
+        recordingInProgressLabel.hidden = false
+        recordButton.enabled = false
+        //TODO: record audio
+     
+    
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        recordingInProgressLabel.hidden = true
+        recordButton.enabled = true
+        //TODO: stop recording
+        
+    }
+  
 
 }
 
